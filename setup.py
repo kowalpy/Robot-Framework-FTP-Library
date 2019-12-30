5bfeef65f816b42d1aa30d2f4d39fc2738ad9e93
kowalpy <mkov80@gmail.com>
2019-12-30 20:47:26 +0100

    yet another piece of code where version number needs to be updated

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ To install Robot Framework Ftp Library execute command:
 from distutils.core import setup
 
 setup(name='robotframework-ftplibrary',
-      version='1.6',
+      version='1.7',
       description='Robot Framework Ftp Library',
       author='Marcin Kowalczyk',
       license='GPLv3',
